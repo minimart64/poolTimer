c7eb3f01d7f9c5f1b396d19eb2067ba850f46467
“minimart” <minimart@me.com>
2017-12-16 20:43:48 -0500

    more methods... still not complete

--- poolTimer.py
+++ poolTimer.py
@@ -21,30 +21,59 @@ heaterPin = 17
 lightPin = 27
 fillPin = 22
 
-# set up the pins
-# GPIO.setwarnings(False) # add in when ready to go to prod
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pumpPin, GPIO.OUT)
-GPIO.setup(heaterPin, GPIO.OUT)
-GPIO.setup(lightPin, GPIO.OUT)
-GPIO.setup(fillPin, GPIO.OUT)
+def setUp():
+    # set up the pins
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pumpPin, GPIO.OUT)
+    GPIO.setup(heaterPin, GPIO.OUT)
+    GPIO.setup(lightPin, GPIO.OUT)
+    GPIO.setup(fillPin, GPIO.OUT)
 
-#set them all LOW
-GPIO.output(pumpPin, GPIO.LOW)
-GPIO.output(heaterPin, GPIO.LOW)
-GPIO.output(lightPin, GPIO.LOW)
-GPIO.output(fillPin, GPIO.LOW)
-
-
-
-# cycle some stuff
-GPIO.output(heaterPin, GPIO.HIGH)
-sleep(10)
-GPIO.output(heaterPin, GPIO.LOW)
+def allOff():
+    #set them all LOW
+    GPIO.output(pumpPin, GPIO.LOW)
+    GPIO.output(heaterPin, GPIO.LOW)
+    GPIO.output(lightPin, GPIO.LOW)
+    GPIO.output(fillPin, GPIO.LOW)
 
 def heaterOn():
-	pumpOn()
-	GPIO.output(heaterPin, GPIO.HIGH)
-	
+    pumpOn() # can't run heater without pump
+    GPIO.output(heaterPin, GPIO.HIGH)
+    log.info("Heater On")
+
+def heaterOff():
+    GPIO.output(heaterPin, GPIO.LOW)
+    log.info("Heater Off")
+    
 def pumpOn():
-	
+    GPIO.output(pumpPin, GPIO.HIGH)
+    log.info("Pump On")
+
+def pumpOff():
+    heaterOff() # can't run heater without pump
+    GPIO.output(pumpPin, GPIO.LOW)
+    log.info("Pump Off")
+
+def lightOn():
+    GPIO.output(lightPin, GPIO.HIGH)
+    log.info("Light On")
+
+def lightOff():
+    GPIO.output(lightPin, GPIO.LOW)
+    log.info("Light Off")
+
+def fillOn():
+    GPIO.output(fillPin, GPIO.HIGH)
+    log.info("Fill On")
+    sleep(3600)
+    fillOff()
+
+def fillOff():
+    GPIO.output(fillPin, GPIO.LOW)
+    log.info("Fill Off")
+
+# actual code starts here
+setUp()
+heaterOff()
+fillOn()
